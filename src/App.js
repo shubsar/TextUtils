b43328eb8ca252c523f18a055770a5b8c6961912
sudhendra <contactwithsid@gmail.com>
2025-01-12 13:51:44 +0530

    Fixed router issues and fixed link component urls

--- src/App.js
+++ src/App.js
@@ -1,94 +1,60 @@
-
 import { useState } from "react";
 import About from "./About";
 import "./App.css";
 import Navbar from "./Navbar";
 import Textform from "./Textform";
 import Alert from "./components.js/Alert";
-import { createBrowserRouter,RouterProvider,BrowserRouter as Router,Switch,Route,Link, BrowserRouter } from "react-router-dom";
-// import API from "./practice";
-let myStyle={
-  color:"red",
-  BackgroundColor:"blue"
-}
-
-
-
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-   const [mode,setMode]=useState('light')
-   const[alert,setAlert]=useState(null);
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-   const showAlert=(message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      message:message,
-      type:type
-
-    })
-    setTimeout(()=>{
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
       setAlert(null);
+    }, 1500);
+  };
 
-    },1500)
-   }
-
-
-
-   const toggleMode=()=>{
-    if(mode==='light'){
-      setMode('dark');
-      document.body.style.backgroundColor='#60628b';
-      showAlert('Dark mode has been enabled.','success')
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#60628b";
+      showAlert("Dark mode has been enabled.", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled.", "success");
     }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert('Light mode has been enabled.',"success")
-    }
-  }
+  };
 
-  const router=createBrowserRouter([
-    {path:"/",element: <Textform showAlert={showAlert} heading="Enter the text to analyze"mode={mode}/>},
-    {path:"/",element: <About/>}
-
-  ])
-  return(
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Navbar title={"TextUtils"} mode={mode} toggleMode={toggleMode} />
+          <Textform
+            showAlert={showAlert}
+            heading="Enter the text to analyze"
+            mode={mode}
+          />
+        </>
+      ),
+    },
+    { path: "/about", element: <About /> },
+  ]);
+  return (
     <>
-    
-   {/* <Router> */}
-    <Navbar title={"TextUtils"} mode={mode} toggleMode={toggleMode}/>
-    <Alert alert={alert}/>
-    <RouterProvider router={router}/>
-    <div className="container my-3 ">
-    {/* <Textform showAlert={showAlert} heading="Enter the text to analyze"mode={mode}/> */}
-    {/* <div className="container">
-      <Switch>
-        <Route path="/about">
-        <About/>
-        
-        </Route>
-       < Route path="/">
-       <Textform showAlert={showAlert} heading="Enter the text to analyze"mode={mode}/>
-        
-        
-        </Route>
-
-      </Switch> */}
-  
-
-    {/* </div> */}
-   
-
-
-    </div>
-    {/* </Router> */}
-    {/* <API/> */}
-    
-   
-    
+      <RouterProvider router={router}>
+        <Alert alert={alert} />
+      </RouterProvider>
     </>
-  )
+  );
 }
-  
+
 export default App;
